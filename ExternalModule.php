2e9314401989d1a41bd53029a8131aeddeef4906
prasadlan <prasad2311@gmail.com>
2017-10-31 13:15:27 -0400

    changes to handle project settings config changes

--- ExternalModule.php
+++ ExternalModule.php
@@ -20,8 +20,26 @@ class ExternalModule extends AbstractExternalModule {
     function hook_save_record($project_id, $record, $instrument, $event_id, $group_id) {
         include_once 'includes/enroll_helper.php';
         
-        // get project settings from configuration data of the project.
-        $project_settings = json_decode($this->getProjectSetting("enroll-into-arm-module-settings", $project_id));
+        // Get project settings from configuration data of the project.
+        $output = ExternalModules::getProjectSettingsAsArray('enroll_into_arm', $project_id);
+        $project_settings = array();
+
+        // Create output as an array.
+        foreach ($output as $key => $value) {
+            $project_settings[$key] = $value['value'];
+        }
+
+        // Nesting elements under randomization_field_values.
+        foreach ($project_settings['randomization_field_values'] as $key => $value) {
+            $inner = array();
+            $inner['value'] = $project_settings['value'][$key];
+            $inner['arm_to_enroll'] = $project_settings['arm_to_enroll'][$key];
+            $project_settings['randomization_field_values'][$key] = $inner;
+        }
+
+        unset($project_settings['value']);
+        unset($project_settings['arm_to_enroll']);
+
         enroll_into_arm_enroll_helper($project_id, $project_settings, $record, $event_id);
     }
 }
