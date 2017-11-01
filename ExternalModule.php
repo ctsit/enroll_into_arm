686ab0a926cf2e6e57bbf843420ef366129d4bed
Tiago Bember Simeao <tbembersimeao@ufl.edu>
2017-11-01 15:09:55 -0400

    Implementing a generic way to format a REM settings output.

--- ExternalModule.php
+++ ExternalModule.php
@@ -19,27 +19,60 @@ class ExternalModule extends AbstractExternalModule {
      */
     function hook_save_record($project_id, $record, $instrument, $event_id, $group_id) {
         include_once 'includes/enroll_helper.php';
-        
-        // Get project settings from configuration data of the project.
-        $output = ExternalModules::getProjectSettingsAsArray('enroll_into_arm', $project_id);
-        $project_settings = array();
+        enroll_into_arm_enroll_helper($project_id, $this->getFormattedSettings($project_id), $record, $event_id);
+    }
 
-        // Create output as an array.
-        foreach ($output as $key => $value) {
-            $project_settings[$key] = $value['value'];
+    /**
+     * Formats settings into a hierarchical key-value pair array.
+     *
+     * @param int $project_id
+     *   Enter a project ID to get project settings.
+     *   Leave blank to get system settings.
+     *
+     * @return array $formmated
+     *   The formatted settings.
+     */
+    function getFormattedSettings($project_id = null) {
+        $config = $this->getConfig();
+
+        if ($project_id) {
+            $type = 'project';
+            $settings = ExternalModules::getProjectSettingsAsArray($this->PREFIX, $project_id);
+        }
+        else {
+            $type = 'system';
+            $settings = ExternalModules::getSystemSettingsAsArray($this->PREFIX);
         }
 
-        // Nesting elements under randomization_field_values.
-        foreach ($project_settings['randomization_field_values'] as $key => $value) {
-            $inner = array();
-            $inner['value'] = $project_settings['value'][$key];
-            $inner['arm_to_enroll'] = $project_settings['arm_to_enroll'][$key];
-            $project_settings['randomization_field_values'][$key] = $inner;
+        $formatted = array();
+        foreach ($config[$type . '-settings'] as $field) {
+            $key = $field['key'];
+
+            if ($field['type'] == 'sub_settings') {
+                // Handling sub settings.
+                $formatted[$key] = array();
+
+                if ($field['repeatable']) {
+                    // Handling repeating sub settings.
+                    foreach (array_keys($settings[$key]['value']) as $delta) {
+                        foreach ($field['sub_settings'] as $sub_setting) {
+                            $sub_key = $sub_setting['key'];
+                            $formatted[$key][$delta][$sub_key] = $settings[$sub_key]['value'][$delta];
+                        }
+                    }
+                }
+                else {
+                    foreach ($field['sub_settings'] as $sub_setting) {
+                        $sub_key = $sub_setting['key'];
+                        $formatted[$key][$sub_key] = reset($settings[$sub_key]['value']);
+                    }
+                }
+            }
+            else {
+                $formatted[$key] = $settings[$key]['value'];
+            }
         }
 
-        unset($project_settings['value']);
-        unset($project_settings['arm_to_enroll']);
-
-        enroll_into_arm_enroll_helper($project_id, $project_settings, $record, $event_id);
+        return $formatted;
     }
 }
